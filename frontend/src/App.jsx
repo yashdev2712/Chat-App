8e873ae796e470968649cae83dce435f894f4989
yashdev2712 <deveshchandra2712@gmail.com>
2024-06-07 10:04:25 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./index.css";
+import Home from "./pages/Home/Home";
+import Login from './pages/login/Login';
+import Signup from "./pages/signup/Signup";
 
 function App() {
     return (
         <>
+            <div className='p-4 h-screen flex items-center justify-center'>
+                <Login />
+                <Signup />
+                <Home />
 
+            </div>
         </>
     )
 }
